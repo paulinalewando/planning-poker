972a24d550b35478bf2c973d00674c7cedd09a08
paulina <paulinalewando@gmail.com>
2021-01-17 12:49:48 +0100

    Eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,33 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: ['prettier'],
+  ignorePatterns: ['node_modules/'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/attributes-order': 'error',
+    'vue/order-in-components': 'error',
+    'vue/attribute-hyphenation': 'error'
+  }
 }

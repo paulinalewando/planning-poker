d552d617693838113dbc84eb2ee8a7e6de4890f0
paulina <paulinalewando@gmail.com>
2021-01-17 18:55:21 +0100

    socet.io

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,70 +1,74 @@
-import colors from 'vuetify/es5/util/colors'
-
-export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+module.exports = {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: '%s - planning-poker',
-    title: 'planning-poker',
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
   css: [],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    { src: '~/plugins/socket.client.js' },
+    { src: '~/plugins/vuetify.js' }
+  ],
+  /*
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify'
   ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa'],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content'
-  ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
-
-  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+  pwa: {
+    manifest: {
+      name: 'Planning poker',
+      short_name: 'Planning poker',
+      start_url: '/',
+      theme_color: '#932d53',
+      display: 'standalone'
+    },
+    icon: {
+      iconSrc: './static/favicon.ico'
     }
   },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
 }

c8e27ef50d331861ac7067273e5b81beae569dad
paulina <paulinalewando@gmail.com>
2021-01-24 13:24:35 +0100

    Fix test

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import AppLogo from '@/components/AppLogo.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(AppLogo)
     expect(wrapper.vm).toBeTruthy()
   })
 })
